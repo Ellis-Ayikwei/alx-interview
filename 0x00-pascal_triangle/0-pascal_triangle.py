6b33610002c850d52b94b98e0ceb9b115c16d24f
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-07-28 18:42:10 +0000

    "Added two blank lines to pascal_triangle function docstring"

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,8 @@
 0-pascal_triangle - function that returns a list of lists
 of integers representing the Pascal’s triangle of n
 """
+
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing the
