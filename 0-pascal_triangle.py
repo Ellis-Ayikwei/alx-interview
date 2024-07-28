6e236ab517aae1ee7399b6260d9d122e1930fe07
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-07-28 18:29:29 +0000

    created the pascals triangle using the list comprehension

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal's triangle of n.
+
+    Args:
+        n (int): the number of rows in Pascal's triangle
+
+    Returns:
+        list: a list of lists of integers representing the Pascal's triangle
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]] 
+
+    for i in range(1, n):
+        row = [1] + [triangle[i-1][j-1] + triangle[i-1][j] for j in range(1, i)] + [1]
+        triangle.append(row)  
+
+    return triangle
