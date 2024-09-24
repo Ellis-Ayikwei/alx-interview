e2b68e3603bec29c12c31ea137d1c8f497346c95
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-24 10:22:54 +0000

    the pycode fixed

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,14 +3,17 @@
 
 
 def makeChange(coins, total):
-    """Return the minimum number of coins needed to make change for `total` using
-    the given `coins` denominations. If no combination of coins sums up to `total`,
+    """Return the minimum number of coins needed to make change
+    for `total` using
+    the given `coins` denominations. If no combination of coins sums
+    up to `total`,
     return -1.
     """
     if total <= 0:
         return 0
 
-    # Initialize an array of size `total + 1` with all elements set to infinity,
+    # Initialize an array of size `total + 1` with all elements
+    # set to infinity,
     # except for the first element which is 0.
     dp = [float("inf")] * (total + 1)
     dp[0] = 0
@@ -23,6 +26,7 @@ def makeChange(coins, total):
             # `dp[amount - coin] + 1`.
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    # If `dp[total]` is still infinity, then no combination of coins sums up to
+    # If `dp[total]` is still infinity, then no combination
+    # of coins sums up to
     # `total`, so return -1. Otherwise, return `dp[total]`.
     return dp[total] if dp[total] != float("inf") else -1
