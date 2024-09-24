276109b8f7449bcc7892be54eab0ef6a669f02c0
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-24 10:34:44 +0000

    remade it mmore efficient

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,21 +9,22 @@ def makeChange(coins, total):
     up to `total`,
     return -1.
     """
+    if total <= 0:
+        return 0
+
     # Initialize an array of size `total + 1` with all elements
     # set to infinity,
     # except for the first element which is 0.
     dp = [float("inf")] * (total + 1)
     dp[0] = 0
 
-    # Iterate over each amount from 1 to `total`.
-    for amount in range(1, total + 1):
-        # Iterate over each coin denomination.
-        for coin in coins:
-            # If `coin` is less than or equal to `amount`, then
-            # `dp[amount]` is the
-            # minimum of its current value and `dp[amount - coin] + 1`.
-            if coin <= amount:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    # Iterate over each coin denomination.
+    for coin in coins:
+        # Iterate over each amount from `coin` to `total`.
+        for amount in range(coin, total + 1):
+            # Update `dp[amount]` to be the minimum of its current value and
+            # `dp[amount - coin] + 1`.
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
     # If `dp[total]` is still infinity, then no combination
     # of coins sums up to
